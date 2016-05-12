936c99af7a6196d613dc231f28f9b159da917094
Vigith Maurice <vigith@gmail.com>
2016-05-11 18:27:14 -0700

    add all the functions to header

--- parse_time.h
+++ parse_time.h
@@ -1,9 +1,10 @@
 #include "vmplot.h"
 
-
-
+/* Variables */
 extern char* time_format_hints[];
 extern size_t time_format_hint_cnt;
 
+/* Functions */
 extern unsigned int get_format_for_time(const char *);
-time_t convert_time_to_epoch(const char *, int, const char *);
+extern time_t convert_time_to_epoch(const char *, int, const char *);
+extern int convert_epoch_to_time(time_t, int, const char *, char *, size_t);
