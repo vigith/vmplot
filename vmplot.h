10cf6d93174a73c57fbc5cb822db129d0380eb5b
Vigith Maurice <vigith@gmail.com>
2016-05-14 14:40:38 -0700

    status code generic, use custom ERROR codes

--- vmplot.h
+++ vmplot.h
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <errno.h>
+
+/* STATUS Codes */
+#define SUCCESS 1
+#define FAILURE 0
 
 /* ERRORS */
-#define ERANGE     1          /* out of bound array (<0 or >length) */
-#define EPARSETIME 2          /* cannot parse time */
-#define ETIMECONV  3          /* cannot do time conversion */
-#define ETIMEFMT   4          /* time formatting failed */
+#define E_VM_RANGE     1          /* out of bound array (<0 or >length) */
+#define E_VM_PARSETIME 2          /* cannot parse time */
+#define E_VM_TIMECONV  3          /* cannot do time conversion */
+#define E_VM_TIMEFMT   4          /* time formatting failed */
 
-/* any system that returns error, should call clr_error before processing */
-#define clr_error() do { vmplot_errno = 0; vmplot_errstr[0] = '\0'; } while (0);
+/* any system that sets error code, should call clr_error before processing */
+#define clr_error() do { vmplot_errno = 0; vmplot_errstr[0] = '\0'; } while (0)
+/* set the error code and error string */
 #define set_error(_errno, _errstr) do { vmplot_errno = _errno; strcpy(vmplot_errstr, _errstr); } while (0)
 
 int vmplot_errno;
