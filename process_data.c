8e3dcd0278e5877f35bf3a935d9fd16b271436f9
Vigith Maurice <vigith@gmail.com>
2016-05-31 22:08:30 -0700

    basic processing

---
+++ process_data.c
@@ -0,0 +1,109 @@
+/* process data that is provided to plot the grapht */
+
+#include "process_data.h"
+
+/* set the values in data2axis int array using the values
+   in opt_data2axis. 
+   Args: None
+   Returns:
+    - SUCCESS
+    - FAILURE
+   ERROR: will set vmplot_errno and vmplot_errstr on error
+*/
+int set_data2axis(void) {
+  /* initial all the value to 1 */
+  int xd = 1,                   /* X_DOWN */
+    yl = 1,                     /* Y_LEFT */
+    yr = 1,                     /* Y_RIGHT */
+    xt = 1;                     /* X_TOP */
+  
+  int retcode = FAILURE;        /* return status */
+  char *saveptr;                /* reentrant tokenizer ; between axes ..D;L.. */
+  char *tokenptr;               /* pointer to next token */
+  char *tmpstr;                 /* tmp string for strtok */
+  int field = 0;                /* current field worked upon in data2axis */
+  
+  /* clear error */
+  clr_error();
+
+
+  /* we need to make sure opt_data2axis has some valid str */
+  if (strlen(opt_data2axis) == 0) {
+    set_error(E_VM_WRONGVAL, "opt_data2axis cannot be empty");
+    return retcode;
+  }
+
+  /* free this memory, strdup malloc's */
+  tmpstr = strdup(opt_data2axis);
+
+  /* tokenize the string and this is never NULL
+     XXX: we will always have something at X and Y axis,
+     in case user asks to plot an 1 variable graph then
+     x-axis will be just a counter.
+   */
+  tokenptr = strtok_r(tmpstr, ";", &saveptr);
+  do {
+    if (*tokenptr == 'D') {
+      data2axis[field++] |= (X_DOWN | (xd << 8)); /* we don't use the MSB it actually */
+      xd <<= 1;
+    } else if (*tokenptr == 'T') { /* we don't use the MSB it actually */
+      data2axis[field++] |= X_TOP;
+      xt << 1;
+    } else if (*tokenptr == 'L') {
+      /* we assume we will get :D or :T after this */
+      if (tokenptr+1 != NULL && *(tokenptr+1) == ':' && tokenptr+2 != NULL) {
+        if (*(tokenptr+2) == 'D') {
+          data2axis[field++] |= (Y_LEFT | (yl << 8) | (X_DOWN << 24)); /* the MSB contains which index we need to add the entry to */
+        } else if (*(tokenptr+2) == 'T') {
+          data2axis[field++] |= (Y_LEFT | (yl << 8) | (X_TOP << 24)); /* the MSB contains which index we need to add the entry to */
+        } else {
+          set_error(E_VM_WRONGVAL, "opt_data2axis expected D/T");
+          break;
+        }
+      } else {
+        set_error(E_VM_WRONGVAL, "opt_data2axis expected : D/T");
+        break;
+      }
+      yl <<= 1;               /* move the next index */      
+    } else if (*tokenptr == 'R') {
+      /* we assume we will get :D or :T after this */
+      if (tokenptr+1 != NULL && *(tokenptr+1) == ':' && tokenptr+2 != NULL) {
+        if (*(tokenptr+2) == 'D') {
+          data2axis[field++] |= (Y_RIGHT | (yr << 8) | (X_DOWN << 24)); /* the MSB contains which index we need to add the entry to */
+        } else if (*(tokenptr+2) == 'T') {
+          data2axis[field++] |= (Y_RIGHT | (yr << 8) | (X_TOP << 24)); /* the MSB contains which index we need to add the entry to */
+        } else {
+          set_error(E_VM_WRONGVAL, "opt_data2axis expected D/T");
+          break;
+        }
+      } else {
+        set_error(E_VM_WRONGVAL, "opt_data2axis expected : D/T");
+        break;
+      }
+      yr <<= 1;
+    } else {
+      set_error(E_VM_WRONGVAL, "opt_data2axis exptected D/T/L/R");
+      break;
+    }
+  } while ((tokenptr = strtok_r(NULL, ";", &saveptr)) != NULL) ;
+
+  /* free the tmpstr allocation */
+  free(tmpstr);
+  
+  return retcode;
+}
+
+
+int main(void) {
+  int status;
+  int i;
+  /* create space for data2axis */
+  data2axis = (int *)malloc(sizeof(int) * opt_fields);
+  bzero(data2axis, opt_fields);
+  status = set_data2axis();
+  for(i=0; i<opt_fields; i++) {
+    printf("%d\n", data2axis[i]);
+  }
+  free(data2axis);
+  return 0;
+}
---
+++ process_data.h
@@ -0,0 +1,14 @@
+#include "vmplot.h"
+#include "store.h"
+
+/* integer array which contains which axis each field goes to and which index to keep
+   for each field (ie, "D;L:D,L:D" means first field is X_DOWN and should be kepth at 
+   index 0, second is Y_LEFT and kept at index 0, 3rd is Y_LEFT and kept at index 1.)
+   each field in data2axis array is of 3 main parts
+    - last 8 bits (LSB) have the axis info
+    - next 16 bits have the index to where in array y_left_arr or y_right_arr (this
+      means we can not have more than 16 plots for left or right y axis)
+    - last 8 bits is to show whether data is plotted again X_DOWN or X_TOP (useful
+      only for Y_LEFT and Y_RIGHT)
+*/
+int *data2axis;
